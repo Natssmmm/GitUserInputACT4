8cd54c21426f151d45d1fe76c2d86517875dd3de
Natss <168500422+Natssmmm@users.noreply.github.com>
2025-08-14 13:03:27 +0800

    Add files via upload

---
+++ Act4.dart/Act4.dart.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+// void main(){
+//     print("Enter your name");
+//     String? name = stdin.readLineSync();
+//     print("Enter your name");
+//     String? section = stdin.readLineSync();
+//     print("Enter your name");
+//     String? course = stdin.readLineSync();
+//     print("Enter your name");
+//     String? year level = stdin.readLineSync();
+//     print("Enter your name");
+//     String? subject = stdin.readLineSync();
+//     print("My name is ${name} from ${section} I chose course is ${course} also my ${year level} nad my subject is ${subject}");
+// }
+
